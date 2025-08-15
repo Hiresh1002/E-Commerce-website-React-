84dc600390ad6e1331028a18ce3c26d3341629b4
Hiresh1002 <harishind198@gmail.com>
2025-08-15 23:49:15 +0530

    jkjbja

--- majorProject/src/component/Home.jsx
+++ majorProject/src/component/Home.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
-const video = "https://player.cloudinary.com/embed/?cloud_name=disi15r7a&public_id=275498_small_pcj7pa&profile=cld-default";
 
 function Home() {
   return (
     <div>
       <h1>hello</h1>
       
-      <video 
-        src={video} 
-        controls 
-        autoPlay 
-        loop 
-        muted 
-        style={{ width: "600px", borderRadius: "12px", marginTop: "20px" }}
-      />
+     
     </div>
   );
 }
